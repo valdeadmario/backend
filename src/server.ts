c7cbdae706a6661db6a0aa4c8251270d1b232074
valdeadmario <vladimir.vstup@gmail.com>
2020-02-19 01:22:06 +0200

    Change port

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import * as OrderService from "./services/order.service";
 import { Order, ErrorResponse } from "./types/order.type";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
